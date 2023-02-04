85d3798150ca9a69231a2e33b59493b4926bffa7
gyovine <fngiovine@gmail.com>
2023-02-04 14:38:08 -0300

    First Commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["Dragon Ball"]);
+
+  const onAddCategory = (NewCategory) => {
+    if (categories.includes(NewCategory)) return;
+
+    setCategories([NewCategory, ...categories]);
+  };
+
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+      <AddCategory onNewCategory={onAddCategory} />
+
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
+    </>
+  );
+};
